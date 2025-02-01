184e31609ed716a6832fe22acc8510d688e4ddb8
Андрей <benderkott@gmail.com>
2025-02-01 23:47:00 +0300

    add users2

--- server.js
+++ server.js
@@ -8,56 +8,15 @@ const fs = require('fs');
 const path = require('path');
 const server = http.createServer(app);
 const io = socketIo(server);
-const jsonParser = express.json();
+// const jsonParser = express.json();
 
 io.on('connection', (socket) => {
     
     socket.on('registerUser', (request, response) => {
         const user = request.body;
-    
-        // Путь к файлу users.json
-        const filePath = path.join(__dirname, 'users.json');
-    
-        // Чтение файла users.json
-        fs.readFile(filePath, 'utf8', (err, data) => {
-            if (err) {
-                console.error('Ошибка чтения файла:', err);
-                response.status(500).send('Ошибка сервера');
-                return;
-            }
-    
-            // Парсинг JSON-данных
-            let usersData;
-            try {
-                usersData = JSON.parse(data);
-            } catch (parseError) {
-                console.error('Ошибка парсинга JSON:', parseError);
-                response.status(500).send('Ошибка сервера');
-                return;
-            }
-    
-            // Проверка на уникальность пользователя
-            const existingUser = usersData.users.find(u => u.userName === user.userName);
-            if (existingUser) {
-                response.status(400).send('Пользователь с таким именем уже существует');
-                return;
-            }
-    
-            // Добавление нового пользователя
-            usersData.users.push(user);
-    
-            // Запись обновлённых данных обратно в файл
-            fs.writeFile(filePath, JSON.stringify(usersData, null, 2), 'utf8', (writeErr) => {
-                if (writeErr) {
-                    console.error('Ошибка записи файла:', writeErr);
-                    response.status(500).send('Ошибка сервера');
-                    return;
-                }
-    
-                console.log(`Пользователь ${user.userName} зарегистрирован`);
-                response.status(200).send('Регистрация успешна');
-            });
-        });
+        console.log(user.userName);
+        console.log(user.email);
+        console.log(user.password);
     });
 
     console.log('Пользователь присоединился');
