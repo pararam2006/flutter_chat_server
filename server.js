0385fad32ebe8be5c39825823b720626019a13fc
Андрей <benderkott@gmail.com>
2025-02-04 19:26:18 +0300

    ю

--- server.js
+++ server.js
@@ -1,86 +1,61 @@
-process.env.GOOGLE_APPLICATION_CREDENTIALS = './flutter-sockets-firebase-adminsdk-fbsvc-67d88f4a99.json'
-const admin = require('firebase-admin');
-const { initializeApp, cert, applicationDefault } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
-const serviceAccount = require('./flutter-sockets-firebase-adminsdk-fbsvc-67d88f4a99.json');
-
-console.log('Initializing Firebase with service account:', serviceAccount);
-
-admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-    credential: applicationDefault()
-});
-
-const db = getFirestore();
+const fs = require('fs');
+const path = require('path');
 const { Socket } = require('dgram');
 const http = require('http');
 const express = require('express');
-const path = require('path');
 const socketIo = require('socket.io');
+
 const server = http.createServer(express());
 const io = socketIo(server);
 
-const ntpClient = require('ntp-client');
+function addUserToFile(user) {
+    const filePath = path.join(__dirname, 'users.json');
 
-ntpClient.getNetworkTime("pool.ntp.org", 123, (err, date) => {
-    if (err) {
-        console.error("Ошибка при синхронизации времени:", err);
-        return;
-    }
-    console.log("Текущее время (синхронизировано с NTP):", date);
-});
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Ошибка при чтении файла:', err);
+            return;
+        }
 
-io.on('connection', (socket) => {
-    
-    socket.on('registerUser', async (user) => {
+        let usersData;
         try {
-            console.log('Registering user:', user);
-            const userDoc = await db.collection('users').doc(user.userName).get();
-    
-            if (userDoc.exists) {
-                const errMsg = `Пользователь ${user.userName} уже зарегистрирован`;
-                io.emit('err', errMsg);
-                console.log(errMsg);
+            usersData = JSON.parse(data);
+        } catch (parseErr) {
+            console.error('Ошибка при парсинге JSON:', parseErr);
+            return;
+        }
+
+        const userExists = usersData.users.some(existingUser => existingUser.userName === user.userName);
+        if (userExists) {
+            console.log(`Пользователь ${user.userName} уже зарегистрирован`);
+            return;
+        }
+
+        usersData.users.push(user);
+
+        fs.writeFile(filePath, JSON.stringify(usersData, null, 2), 'utf8', (writeErr) => {
+            if (writeErr) {
+                console.error('Ошибка при записи в файл:', writeErr);
                 return;
             }
-    
-            await db.collection('users').doc(user.userName).set({
-                email: user.email,
-                password: user.password             
-            });
-            console.log(`Пользователь ${user.userName} зарегистрирован`);
-        } 
-        catch(err) {
-            const errMsg = `Ошибка при регистрации: ${err}`;
-            io.emit('err', errMsg);
-            console.log(errMsg);
-        }
+            console.log(`Пользователь ${user.userName} успешно зарегистрирован и записан в файл`);
+        });
     });
-    
+}
 
+io.on('connection', (socket) => {
     console.log('Пользователь присоединился');
 
-    socket.on('first', async () => {
-        console.log('first');
-        try {
-            const docRef = db.collection('test').doc('testDoc');
-            await docRef.set({ test: 'test' });
-            console.log('Запись без регистрации работает');
-        } 
-        catch (err){
-            console.log(`Ошибка при записи данных: ${err}`)
-        }
-        io.emit('response', 'Нажата первая кнопка');
-    });
-
-    socket.on('second', () => {
-        console.log('second');
-        io.emit('response', 'Нажата вторая кнопка');
+    socket.on('registerUser', (user) => {
+        console.log('Registering user:', user);
+        addUserToFile(user);
     });
 
     socket.on('disconnect', () => {
         console.log('Пользователь отсоединился');
     });
-})
+});
 
-server.listen(() => {console.log(`Сервер работает на https://flutter-chat-server-q7ne.onrender.com`)});
+server.listen(() => {
+    console.log('Сервер работает на https://flutter-chat-server-q7ne.onrender.com');
+});
