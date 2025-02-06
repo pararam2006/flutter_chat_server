17395bc3c4cabecde1929c29427008740d045c17
Андрей <benderkott@gmail.com>
2025-02-06 15:58:48 +0300

    restore firebase adminsdk

--- server.js
+++ server.js
@@ -1,54 +1,86 @@
-const fs = require('fs')
-const path = require('path')
-const http = require('http')
-const express = require('express')
-const socketIo = require('socket.io')
-const mysql = require('mysql2/promise') 
-const app = express()
-const server = http.createServer(app)
-const io = socketIo(server)
-const PORT = 10000
+process.env.GOOGLE_APPLICATION_CREDENTIALS = './flutter-sockets-firebase-adminsdk-fbsvc-47cb93817a.json'
+const admin = require('firebase-admin');
+const { initializeApp, cert, applicationDefault } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+const serviceAccount = require('./flutter-sockets-firebase-adminsdk-fbsvc-47cb93817a.json');
 
-const dbConfig = {
-    host: 'sql.freedb.tech',
-    port: 3306,
-    user: 'freedb_bebebe',
-    password: '4Fn#Wz3PV8XCG**',
-    database: 'freedb_flutter-chat-db' 
-}
+console.log('Initializing Firebase with service account:', serviceAccount);
 
-const pool = mysql.createPool(dbConfig);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+    // credential: applicationDefault()
+});
 
-async function addUserToDatabase(user) {
-    try {
-        const [rows] = await pool.execute('SELECT * FROM users WHERE userName = ?', [user.userName]);
-        if (rows.length > 0) {
-            console.log(`Пользователь ${user.userName} уже зарегистрирован.`);
-            return;
-        }
+const db = getFirestore();
+const { Socket } = require('dgram');
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const socketIo = require('socket.io');
+const server = http.createServer(express());
+const io = socketIo(server);
 
-        await pool.execute('INSERT INTO users (userName, email, password) VALUES (?, ?, ?)', 
-            [user.userName, user.email, user.password]);
-        console.log(`Пользователь ${user.userName} успешно зарегистрирован.`);
-    } catch (error) {
-        console.error('Ошибка при работе с базой данных:', error);
+const ntpClient = require('ntp-client');
+
+ntpClient.getNetworkTime("pool.ntp.org", 123, (err, date) => {
+    if (err) {
+        console.error("Ошибка при синхронизации времени:", err);
+        return;
     }
-}
-
+    console.log("Текущее время (синхронизировано с NTP):", date);
+});
 
 io.on('connection', (socket) => {
-    console.log('Пользователь присоединился')
-
+    
     socket.on('registerUser', async (user) => {
-        console.log('Регистрация пользователя:', user)
-        await addUserToDatabase(user) 
-    })
+        try {
+            console.log('Registering user:', user);
+            const userDoc = await db.collection('users').doc(user.userName).get();
+    
+            if (userDoc.exists) {
+                const errMsg = `Пользователь ${user.userName} уже зарегистрирован`;
+                io.emit('err', errMsg);
+                console.log(errMsg);
+                return;
+            }
+    
+            await db.collection('users').doc(user.userName).set({
+                email: user.email,
+                password: user.password             
+            });
+            console.log(`Пользователь ${user.userName} зарегистрирован`);
+        } 
+        catch(err) {
+            const errMsg = `Ошибка при регистрации: ${err}`;
+            io.emit('err', errMsg);
+            console.log(errMsg);
+        }
+    });
+    
+
+    console.log('Пользователь присоединился');
+
+    socket.on('first', async () => {
+        console.log('first');
+        try {
+            const docRef = db.collection('test').doc('testDoc');
+            await docRef.set({ test: 'test' });
+            console.log('Запись без регистрации работает');
+        } 
+        catch (err){
+            console.log(`Ошибка при записи данных: ${err}`)
+        }
+        io.emit('response', 'Нажата первая кнопка');
+    });
+
+    socket.on('second', () => {
+        console.log('second');
+        io.emit('response', 'Нажата вторая кнопка');
+    });
 
     socket.on('disconnect', () => {
-        console.log('Пользователь отсоединился')
-    })
+        console.log('Пользователь отсоединился');
+    });
 })
 
-server.listen(PORT, () => {
-    console.log(`Сервер работает на https://flutter-chat-server-q7ne.onrender.com:${PORT}`)
-})
+server.listen(() => {console.log(`Сервер работает на https://flutter-chat-server-q7ne.onrender.com`)});
